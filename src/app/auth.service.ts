8174128c5a4644c1a44e15f1151a3b0563c69150
lucas <lucas.zrc@gmail.com>
2019-08-15 16:36:19 -0300

    432

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
+
+interface myData {
+    sucess: boolean,
+    message: string
+}
 @Injectable({
     providedIn: 'root'
 })
@@ -10,12 +15,12 @@ export class AuthService {
     getUserDetails(username, password){
         //POST(!!) detalhes para API retornar as informações SE correto
         
-        return this.http.post('/api/auth.php', {
+        return this.http.post<myData>('/api/auth.php', {
             username,
             password
         }).subscribe(
             data => {
                 console.log(data, "é o que vem do servis") 
-            })
+            });
     }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { Router } from "@angular/router/src/router";
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
@@ -7,17 +8,17 @@ import { AuthService } from '../auth.service';
 })
 export class LoginComponent implements OnInit {
 
-    constructor(private Auth: AuthService) { }
+    constructor(private Auth: AuthService, private router: Router) { }
 
     ngOnInit() {
     }
     loginUser(event){
-        event.preventDefault()
-        const target = event.target
-        const username = target.querySelector('#username').value
-        const password = target.querySelector('#password').value
-        this.Auth.getUserDetails(username, password)
-        console.log(event)
+        event.preventDefault();
+        const target = event.target;
+        const username = target.querySelector('#username').value;
+        const password = target.querySelector('#password').value;
+        this.Auth.getUserDetails(username, password);
+        console.log(event);
     }
 
 }
